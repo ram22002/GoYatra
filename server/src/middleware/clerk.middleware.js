84b50cc5febfa8c2b00edec5a043cf3c92ececf6
Ram <ramlakhanregar1+github@gmail.com>
2025-09-19 23:47:52 +0530

    server directory

--- server/src/middleware/clerk.middleware.js
+++
@@ -1,6 +0,0 @@
-const { ClerkExpressRequireAuth } = require("@clerk/clerk-sdk-node");
-
-// This is the standard and recommended way to protect routes with Clerk
-const clerkAuthMiddleware = ClerkExpressRequireAuth({});
-
-module.exports = { clerkAuthMiddleware };
